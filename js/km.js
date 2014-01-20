7ca78df2a62879913f27ac9ece0f021527757e57
Kevin Mulvey <king.feruke@gmail.com>
2014-01-20 12:30:49 -0500

    move js vars to file

--- js/km.js
+++ js/km.js
@@ -1,3 +1,7 @@
+var uri ='http://img.kevinmulvey.net/images/';
+var curr_img=14;
+var images=new Array("images/small/seneca.jpg","images/small/bbp.jpg","images/small/halloween_zombie.jpg","images/small/bb_waterfall.jpg","images/small/olivia.jpg","images/small/event_horizon.jpg","images/small/lights.jpg","images/small/farted.jpg","images/small/grapes.jpg","images/small/top_rock_s.jpg","images/small/statue.jpg","images/small/statue_sunset.jpg","images/small/rockafeller.jpg","images/small/willie_wonka.jpg","images/small/lake_placid.jpg","images/small/metrocard.jpg","images/small/erp.jpg","images/small/guutz.jpg","images/small/rock_ctr.jpg","images/small/long_island.jpg","images/small/central_park_fall.jpg","images/small/180ml.jpg","images/small/snowman.jpg","images/small/pepsi.jpg","images/small/dc.jpg");		
+
 $(document).ready(function(){
   // fire image load from menu
   $('a.dock-item').on('click', function(event) {
