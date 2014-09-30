2a6e3b7cb5474c9b986d287278b017e75d62d500
Kevin Mulvey <king.feruke@gmail.com>
2014-09-29 21:58:46 -0400

    remove comments in css

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,6 +31,9 @@ module.exports = function(grunt) {
 					}
 				},
 				cssmin: {
+					options: {
+						keepSpecialComments: 0
+					},
 					combine: {
           	files: {
           		'dist/css/km.<%= pkg.version %>.css': ['css/normalize.css', 'css/km.css']
