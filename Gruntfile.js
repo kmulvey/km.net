93d06b9183ce9e698760ccdff2684115b3c8adc5
Kevin Mulvey <king.feruke@gmail.com>
2014-10-26 08:08:33 -0400

    formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 			meta: {
-    		banner: 
+				banner: 
             '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
             '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
             '* <%= pkg.url %>\n' +
