b0ef9ed63f43d4c21049570f85114785c96a9ca0
Kevin Mulvey <king.feruke@gmail.com>
2014-12-18 12:15:18 -0500

    formatting

--- new/Gruntfile.js
+++ new/Gruntfile.js
@@ -9,18 +9,10 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-bump');
-		// Project configuration.
-		grunt.initConfig({
-			pkg: grunt.file.readJSON('package.json'),
-        meta: {
-    		banner: 
-            '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-            '* <%= pkg.url %>\n' +
-            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.copyright %> ' +
-            ' Licensed MIT */'
-        },
-				clean: {
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+        	clean: {
           js: ["dist/js"],
 					css: ["dist/css"],
 					vendor: ["vendor"]
