a062d5579b3d61228fbcae0ed5e21787e0ec5ac8
Kevin Mulvey <kevin@kevinmulvey.net>
2014-12-21 15:51:13 -0500

    new images

--- new/js/km.js
+++ new/js/km.js
@@ -1,6 +1,4 @@
-var body_width = $('body').width();
-var img_width = $('img').width();
-
+$(document).ready(function(){
 function debounce(func, wait, immediate) {
 	var timeout;
 	return function() {
@@ -17,28 +15,29 @@ function debounce(func, wait, immediate) {
 }
 function calcZoom(){
 	var zoom = 1;
-	var ratio = body_width/img_width;
+	var ratio = $('body').width()/$('picture').width();
 	if(ratio < 1) zoom = ratio;
-	return zoom*100 + "%";
+	return zoom;
 }
 var bouncedZoom = debounce(function() {
-	body_width = $('body').width();
-	$('body').css('zoom', calcZoom());
+	$('body').css('zoom', calcZoom()*100 + "%");
+	$('.helper, picture, img').css('-moz-transform', 'scale(' + calcZoom() + ')');
+	// this can break things
+	$('.helper, picture, img').css('-moz-transform-origin', '0 0');
 }, 250);
 
 // do it once
-$('body').css('zoom', calcZoom());
+bouncedZoom();
 // do it again on resize
 $(window).on('resize', bouncedZoom);
 
 
 // =================================================================================
 
-var uri ='http://img.kevinmulvey.net/images/';
-var curr_img=14;
-var images=new Array("images/small/seneca.jpg","images/small/bbp.jpg","images/small/halloween_zombie.jpg","images/small/bb_waterfall.jpg","images/small/olivia.jpg","images/small/event_horizon.jpg","images/small/lights.jpg","images/small/farted.jpg","images/small/grapes.jpg","images/small/top_rock_s.jpg","images/small/statue.jpg","images/small/statue_sunset.jpg","images/small/rockafeller.jpg","images/small/willie_wonka.jpg","images/small/lake_placid.jpg","images/small/metrocard.jpg","images/small/erp.jpg","images/small/guutz.jpg","images/small/rock_ctr.jpg","images/small/long_island.jpg","images/small/central_park_fall.jpg","images/small/180ml.jpg","images/small/snowman.jpg","images/small/pepsi.jpg","images/small/dc.jpg");		
+var uri ='//img.kevinmulvey.net/images/patagonia/';
+var curr_img=0;
+var images=new Array('IMG_0154.JPG','IMG_0157.JPG','IMG_0167.JPG','IMG_0171.JPG','IMG_0181.JPG','IMG_0187.JPG','IMG_0208.JPG','IMG_0252.JPG','IMG_0262.JPG','IMG_0270.JPG','IMG_0273.JPG','IMG_0284.JPG','IMG_0287.JPG','IMG_0288.JPG','IMG_0311.JPG','IMG_0324.JPG','IMG_0329.JPG','IMG_0335.JPG','IMG_0346.JPG','IMG_0387.JPG','IMG_0414.JPG','IMG_0423.JPG','IMG_0444.JPG','IMG_0447.JPG','IMG_0454.JPG','IMG_0469.JPG','IMG_0490.JPG','IMG_0497.JPG','IMG_0518.JPG','IMG_0519.JPG','IMG_0520.JPG','IMG_0545.JPG','IMG_0547.JPG','IMG_0563.JPG','IMG_0590.JPG','IMG_0593.JPG');
 
-$(document).ready(function(){
   // fire image load from menu
   $('#menu').on('click', 'a', function(event) {
 		var clicked = $(this);
@@ -61,10 +60,10 @@ $(document).ready(function(){
           break;
         }
       }
-    var link = document.location.hash.replace("#/", uri);
+    var link = document.location.hash.replace("#", '');
     var img_wrap = $('.img-wrap');
 		$('picture', img_wrap).remove();
-		img_wrap.append('<picture><source srcset="' + link + '" media="(min-width: 1000px)"><source srcset="' + link + '" media="(min-width: 800px)"><source srcset="' + link + '" media="(min-width: 500px)"><img srcset="' + link + '" alt="180ml"></picture>');
+		img_wrap.append('<picture><source srcset="' + uri + '3k/' + link + '" media="(min-width: 2500px)"><source srcset="' + uri + '2k/' + link + '" media="(min-width: 1500px)"><source srcset="' + uri + '1k/' + link + '" media="(min-width: 500px)"><img srcset="' + uri + '2k/' + link + '" alt="Kevin Mulvey\'s Photography"></picture>');
     e.preventDefault();
     }
   });
