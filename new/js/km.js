fc15a658de7ae7cf236fee8ac7775bb264ad090a
Kevin Mulvey <kmulvey@linux.com>
2015-02-09 07:37:20 -0500

    more keys and formatting

--- new/js/km.js
+++ new/js/km.js
@@ -30,16 +30,14 @@ $(document).ready(function(){
 	}, 250);
 	
 	// do it once
-	//$('body').on("load", 'img', function() {
-	  bouncedZoom();
-	//});
+  bouncedZoom();
 
 
 // =================================================================================
 
-var uri ='//img.kevinmulvey.net/images/patagonia/2k/';
-var curr_img=0;
-var images=new Array('IMG_0154.JPG','IMG_0157.JPG','IMG_0167.JPG','IMG_0171.JPG','IMG_0181.JPG','IMG_0187.JPG','IMG_0208.JPG','IMG_0252.JPG','IMG_0262.JPG','IMG_0270.JPG','IMG_0273.JPG','IMG_0284.JPG','IMG_0287.JPG','IMG_0288.JPG','IMG_0311.JPG','IMG_0324.JPG','IMG_0329.JPG','IMG_0335.JPG','IMG_0346.JPG','IMG_0387.JPG','IMG_0414.JPG','IMG_0423.JPG','IMG_0444.JPG','IMG_0447.JPG','IMG_0454.JPG','IMG_0469.JPG','IMG_0490.JPG','IMG_0497.JPG','IMG_0518.JPG','IMG_0519.JPG','IMG_0520.JPG','IMG_0545.JPG','IMG_0547.JPG','IMG_0563.JPG','IMG_0590.JPG','IMG_0593.JPG','IMG_0620.JPG','IMG_0641.JPG','IMG_0642.JPG','IMG_0656.JPG','IMG_0661.JPG','IMG_0670.JPG','IMG_0680.JPG','IMG_0681.JPG','IMG_0693.JPG','IMG_0695.JPG','IMG_0699.JPG','IMG_0704.JPG','IMG_0710.JPG','IMG_0711.JPG','IMG_0740.JPG','IMG_0746.JPG','IMG_0749.JPG','IMG_0763.JPG','IMG_0770.JPG','IMG_0782.JPG','IMG_0786.JPG','IMG_0789.JPG','IMG_0790.JPG','IMG_0791.JPG','IMG_0801.JPG','IMG_0804.JPG','IMG_0805.JPG','IMG_0807.JPG','IMG_0809.JPG','IMG_0840.JPG','IMG_0866.JPG','IMG_0874.JPG','IMG_0887.JPG','IMG_0893.JPG','IMG_0902.JPG');
+	var uri ='//img.kevinmulvey.net/images/patagonia/2k/';
+	var curr_img=0;
+	var images=new Array('IMG_0154.JPG','IMG_0157.JPG','IMG_0167.JPG','IMG_0171.JPG','IMG_0181.JPG','IMG_0187.JPG','IMG_0208.JPG','IMG_0252.JPG','IMG_0262.JPG','IMG_0270.JPG','IMG_0273.JPG','IMG_0284.JPG','IMG_0287.JPG','IMG_0288.JPG','IMG_0311.JPG','IMG_0324.JPG','IMG_0329.JPG','IMG_0335.JPG','IMG_0346.JPG','IMG_0387.JPG','IMG_0414.JPG','IMG_0423.JPG','IMG_0444.JPG','IMG_0447.JPG','IMG_0454.JPG','IMG_0469.JPG','IMG_0490.JPG','IMG_0497.JPG','IMG_0518.JPG','IMG_0519.JPG','IMG_0520.JPG','IMG_0545.JPG','IMG_0547.JPG','IMG_0563.JPG','IMG_0590.JPG','IMG_0593.JPG','IMG_0620.JPG','IMG_0641.JPG','IMG_0642.JPG','IMG_0656.JPG','IMG_0661.JPG','IMG_0670.JPG','IMG_0680.JPG','IMG_0681.JPG','IMG_0693.JPG','IMG_0695.JPG','IMG_0699.JPG','IMG_0704.JPG','IMG_0710.JPG','IMG_0711.JPG','IMG_0740.JPG','IMG_0746.JPG','IMG_0749.JPG','IMG_0763.JPG','IMG_0770.JPG','IMG_0782.JPG','IMG_0786.JPG','IMG_0789.JPG','IMG_0790.JPG','IMG_0791.JPG','IMG_0801.JPG','IMG_0804.JPG','IMG_0805.JPG','IMG_0807.JPG','IMG_0809.JPG','IMG_0840.JPG','IMG_0866.JPG','IMG_0874.JPG','IMG_0887.JPG','IMG_0893.JPG','IMG_0902.JPG');
 
 	$(window).on('hashchange', function (e) {   
 		console.log(curr_img);
@@ -71,14 +69,14 @@ var images=new Array('IMG_0154.JPG','IMG_0157.JPG','IMG_0167.JPG','IMG_0171.JPG'
 		var code = (e.keyCode ? e.keyCode : e.which);
 		
 		//right
-		if(code === 39) {
+		if(code === 39 || code === 38) {
 			e.preventDefault();
 			if(curr_img >= images.length-1) curr_img = -1;
 			curr_img++;
 			document.location.hash = "#/" + images[curr_img];
 		}
 		//left
-		if(code === 37) {
+		if(code === 37 || code === 40) {
 			e.preventDefault();
 			if(curr_img <= 0) curr_img = images.length;
 			curr_img--;
