9bfc1e8290a2b842d86f1a7ff73082a2cc8ef98c
Kevin Mulvey <kmulvey@linux.com>
2015-02-09 20:36:01 -0500

    jshint rules

--- new/Gruntfile.js
+++ new/Gruntfile.js
@@ -45,39 +45,43 @@ module.exports = function(grunt) {
 			},
 			// ========== CSS ==========
 			csslint: {
-					src: ['css/km.css'],
+				src: ['css/km.css'],
+				options: {
+					ids: false
+				}
+			},
+			cssmin: {
+				dist: {
 					options: {
-						ids: false
-					}
-				},
-				cssmin: {
-					dist: {
-						options: {
-							report: "gzip",
-						},
-						files: {
-							"public/css/dist/km.min.css": ['public/css/vendor/*css', 'public/css/*css']
-						}
-					}
-				},
-				// ========== END CSS ==========
-				// ========== JS ==========
-				jshint: {
-					files: ["public/js/km.js"]
-				},
-
-				uglify: {
-					dist: {
-						files: {
-							'public/js/dist/km.min.js' : 'public/js/km.js'
-						},
-						options:{
-							compress: true,
-							report: 'gzip'
-						}
+						report: "gzip",
+					},
+					files: {
+						"public/css/dist/km.min.css": ['public/css/vendor/*css', 'public/css/*css']
 					}
 				}
-				// ========== END JS ==========
+			},
+			// ========== END CSS ==========
+			// ========== JS ==========
+			jshint: {
+				options: {
+					jshintrc: true,
+					reporter: require('jshint-stylish')
+				},
+				files: ["public/js/km.js"]
+			},
+
+			uglify: {
+				dist: {
+					files: {
+						'public/js/dist/km.min.js' : 'public/js/km.js'
+					},
+					options:{
+						compress: true,
+						report: 'gzip'
+					}
+				}
+			}
+			// ========== END JS ==========
 		});
 
 		// Default task: the works
