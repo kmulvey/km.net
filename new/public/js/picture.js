e391502fc7c645cacef490caacee345cef9ff31a
Kevin Mulvey <kmulvey@linux.com>
2015-04-25 12:30:43 -0400

    fix new visitors

--- new/public/js/picture.js
+++ new/public/js/picture.js
@@ -8,7 +8,11 @@ $(document).ready(function(){
 	var images=new Array('IMG_0154','IMG_0157','IMG_0167','IMG_0171','IMG_0181','IMG_0187','IMG_0208','IMG_0252','IMG_0262','IMG_0270','IMG_0273','IMG_0284','IMG_0287','IMG_0288','IMG_0311','IMG_0324','IMG_0329','IMG_0335','IMG_0346','IMG_0387','IMG_0414','IMG_0423','IMG_0444','IMG_0447','IMG_0454','IMG_0469','IMG_0490','IMG_0497','IMG_0518','IMG_0519','IMG_0520','IMG_0545','IMG_0547','IMG_0563','IMG_0590','IMG_0593','IMG_0620','IMG_0641','IMG_0642','IMG_0656','IMG_0661','IMG_0670','IMG_0680','IMG_0681','IMG_0693','IMG_0695','IMG_0699','IMG_0704','IMG_0710','IMG_0711','IMG_0740','IMG_0746','IMG_0749','IMG_0763','IMG_0770','IMG_0782','IMG_0786','IMG_0789','IMG_0790','IMG_0791','IMG_0801','IMG_0804','IMG_0805','IMG_0807','IMG_0809','IMG_0840','IMG_0866','IMG_0874','IMG_0887','IMG_0893','IMG_0902');
 
 	$(window).on('hashchange', function (e) {   
-		if(document.location.hash.trim() === ""){return;}
+		// first time
+		if(document.location.hash.trim() === ""){
+			$("picture")[0].className = "current";
+			return;
+		}
 		else if(!known){
 			// map the name in the uri to an index in the array
 			var len=images.length;
